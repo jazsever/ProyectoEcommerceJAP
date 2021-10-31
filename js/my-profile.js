d2c8f490febf9dd915c84ab71269f4a49995e2c4
jazsever <jazsever@gmail.com>
2021-10-31 17:19:27 -0300

    Entrega 6

--- js/my-profile.js
+++ js/my-profile.js
@@ -14,5 +14,98 @@ document.addEventListener("DOMContentLoaded", () => {
     </div>
   </div>`
 
+  perfilUsuario = JSON.parse(localStorage.getItem("usuario2"));
+
+
+  document.getElementById("datos_personales").innerHTML = `<div class="card bg-light mb-6">
+  <div class="card-header"></div>
+  <div class="card-body">
+    <h5 class="card-title">Tus datos personales</h5>
+    <p class="card-text">
+      <label>Nombre: `+ perfilUsuario.nombre + `</label>
+      <br>
+      <label>Apellido: `+ perfilUsuario.apellido + `</label>
+      <br>
+      <label>Correo: `+ perfilUsuario.correo + `</label>
+      <br>
+      <label>Fecha de nacimiento: `+ perfilUsuario.fecha + `</label>
+      <br>
+      <label>Celular: `+ perfilUsuario.telefono + `</label>
+      <br>
+
+      <img src="`+perfilUsuario.imagen+`"></img>
+    </p>
+  </div>
+</div>`
+
+  
+
 });
+
+let nombre = document.getElementById("nombre");
+let apellido = document.getElementById("apellido");
+let correo = document.getElementById("mail");
+let fecha = document.getElementById("fecha");
+let telefono = document.getElementById("telefono");
+
+let perfilUsuario = {};
+
+function guardar() {
+
+  perfilUsuario.nombre = nombre.value;
+  perfilUsuario.apellido = apellido.value;
+  perfilUsuario.correo = correo.value;
+  perfilUsuario.fecha = fecha.value;
+  perfilUsuario.telefono = telefono.value;
+  
+  localStorage.setItem('usuario2', JSON.stringify(perfilUsuario));
+  perfil();
+
+};
+
+function perfil() {
+  let htmlContentToAppend = "";
+
+  htmlContentToAppend = `<div class="card bg-light mb-6">
+  <div class="card-header"></div>
+  <div class="card-body">
+    <h5 class="card-title">Tus datos personales</h5>
+    <p class="card-text">
+      <label>Nombre: `+ perfilUsuario.nombre + `</label>
+      <br>
+      <label>Apellido: `+ perfilUsuario.apellido + `</label>
+      <br>
+      <label>Correo: `+ perfilUsuario.correo + `</label>
+      <br>
+      <label>Fecha de nacimiento: `+ perfilUsuario.fecha + `</label>
+      <br>
+      <label>Celular: `+ perfilUsuario.telefono + `</label>
+      <br>
+    </p>
+  </div>
+</div>`
+
+  document.getElementById("datos_personales").innerHTML = htmlContentToAppend;
+}
+
+
+function vistaPrevia() {
+  let preview = document.getElementById('foto');
+  let file = document.querySelector('input[type=file]').files[0];
+  let reader = new FileReader();
+
+  reader.onloadend = function () {
+    preview.src = reader.result;
+    document.getElementById('contenido').innerHTML = reader.result;
+
+  }
+
+  if (file) {
+    reader.readAsDataURL(file);
+
+  } else {
+    preview.src = "img/avatar.png";
+  }
+}
+
 
--- js/products.js
+++ js/products.js
@@ -8,21 +8,16 @@ function showProductsList(array) {
         let product = array[i];
 
         htmlContentToAppend += `
-        <div class="list-group-item list-group-item-action">
-            <div class="row"> 
-                <div class="col-3">
-                    <img src="` + product.imgSrc + `" alt="` + product.description + `" class="img-thumbnail">
-                </div>
-                <div class="col">
-                    <div class="d-flex w-100 justify-content-between">
-                        <h4 class="mb-1">`+ product.name + ` - ` + product.currency + ` ` + product.cost + `</h4>
-                        <small class="text-muted">` + product.soldCount + ` vendidos` + `</small>
-                    </div>
-                        <p class="mb-1">` + product.description + `</p>
-                </div>
-            </div>
-        </div>
-        
+        <div class="col-md-4">
+            <a href="product-info.html" class="card mb-4 shadow-sm custom-card">
+              <img class="bd-placeholder-img card-img-top" src="` + product.imgSrc + `">
+              <h3 class="m-3">`+ product.name + ` -  <br> ` + product.currency + ` ` + product.cost + `</h3>
+              <div class="card-body">
+                <p class="card-text">` + product.description + `</p>
+                <small class="text-muted">` + product.soldCount + ` vendidos` + `</small>
+              </div>
+            </a>
+          </div>       
         `
 
         document.getElementById("cat-list-container").innerHTML = htmlContentToAppend;
